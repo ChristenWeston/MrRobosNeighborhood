2c3a1c2ff514cb267f6921320d5f30a30cea1f44
Christen Weston <paleeserecycle@gmail.com>
2021-11-19 13:55:48 -0800

    Test 6 passing

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@
 function beepBoop(number) {
   const numberToBeep = parseInt(number);
   let numbersAndBeeps = [];
-
   for (let i = 0; i <=numberToBeep; i++) {
     if(i === 0) {
       numbersAndBeeps[i] = 0;

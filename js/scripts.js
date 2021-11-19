38aec0279fcc2da7eea40174136980639cc5e1ca
Christen Weston <paleeserecycle@gmail.com>
2021-11-19 13:17:02 -0800

    Test 5 passing

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,14 @@ function beepBoop(number) {
     if(i === 0) {
       numbersAndBeeps[i] = 0;
     }
-    else if (i > 0 && i.toString().includes("1")) {
-      numbersAndBeeps[i] = "Beep!";
+    else if (i > 0 && i.toString().includes("3")) {
+      numbersAndBeeps[i] = "Won't you be my neighbor?";
     }
     else if (i > 0 && i.toString().includes("2")) {
       numbersAndBeeps[i] = "Boop";
     }
-    else if (i > 0 && i.toString().includes("3")) {
-      numbersAndBeeps[i] = "Won't you be my neighbor?";
+    else if (i > 0 && i.toString().includes("1")) {
+      numbersAndBeeps[i] = "Beep!";
     }
     else {
       numbersAndBeeps[i] = i;
